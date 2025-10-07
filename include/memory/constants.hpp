71ea103688dab24949bc39cc47ebe598f23fd4bf
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2025-10-07 20:53:34 +0200

    UniquesKernel: fixed namespacing for constants

--- include/memory/constants.hpp
+++ include/memory/constants.hpp
@@ -5,21 +5,23 @@
 #include <cstdint>
 #include <cstdalign>
 
+using std::size_t;
+
 namespace anvil {
 namespace memory {
 
-inline constexpr std::size_t EAGER           = std::size_t{1} << 0;
-inline constexpr std::size_t LAZY            = std::size_t{1} << 1;
-inline constexpr std::size_t MAX_ALIGNMENT   = std::size_t{1} << 11; // alignment is capped at half a page.
-inline constexpr std::size_t MIN_ALIGNMENT   = std::size_t{1};
-inline constexpr std::size_t MAX_STACK_DEPTH = std::size_t{64};
+inline constexpr size_t EAGER           = 1 << 0;
+inline constexpr size_t LAZY            = 1 << 1;
+inline constexpr size_t MAX_ALIGNMENT   = 1 << 11; // alignment is capped at half a page.
+inline constexpr size_t MIN_ALIGNMENT   = 1;
+inline constexpr size_t MAX_STACK_DEPTH = 64;
 
 #if SIZE_MAX == UINT32_MAX
 // 32-bit platforms
-inline constexpr std::size_t TRANSFER_MAGIC = static_cast<std::size_t>(0xDEADC0DE);
+inline constexpr size_t TRANSFER_MAGIC = static_cast<size_t>(0xDEADC0DE);
 #elif SIZE_MAX == UINT64_MAX
 // 64-bit platforms
-inline constexpr std::size_t TRANSFER_MAGIC = static_cast<std::size_t>(0xFFFFFFFFDEADC0DE);
+inline constexpr size_t TRANSFER_MAGIC = static_cast<size_t>(0xFFFFFFFFDEADC0DE);
 #else
 #error "Unsupported platform size_t width"
 #endif
