1c4224a4c6f85708d7a187d0d2b93d15841ecedd
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2026-02-26 23:44:12 +0100

    added some examples for stack and lazy stack allocator

--- examples/memory/lazy_stack_memory.cpp
+++ examples/memory/lazy_stack_memory.cpp
@@ -1,11 +1,11 @@
 #include "error/status.hpp"
-#include "memory/stack_allocator.hpp"
+#include "memory/lazy_stack_allocator.hpp"
 #include <cstddef>
 #include <cstdio>
 
 namespace {
-void cleanup_allocator(anvil::memory::stack_allocator::StackAllocator** allocator) {
-        (void)anvil::memory::stack_allocator::destroy(allocator);
+void cleanup_allocator(anvil::memory::lazy_stack_allocator::LazyStackAllocator** allocator) {
+        (void)anvil::memory::lazy_stack_allocator::destroy(allocator);
 }
 } // namespace
 
@@ -14,8 +14,10 @@ int main(void) {
         const size_t                                ALLOC_SIZE = 8; // tiny allocations
 
         // Create allocator with enough space
-        __attribute__((cleanup(cleanup_allocator))) anvil::memory::stack_allocator::StackAllocator* allocator = nullptr;
-        const Error ERR = anvil::memory::stack_allocator::create(&allocator, ALLOC_SIZE * NUM_ALLOCS, alignof(int));
+        __attribute__((cleanup(cleanup_allocator))) anvil::memory::lazy_stack_allocator::LazyStackAllocator* allocator =
+            nullptr;
+        const Error ERR =
+            anvil::memory::lazy_stack_allocator::create(&allocator, ALLOC_SIZE * NUM_ALLOCS, alignof(int));
 
         if (ERR != OK) {
                 printf("ERROR: %i\n", ERR);
@@ -25,8 +27,8 @@ int main(void) {
         // Benchmark: many tiny allocations
         int sum = 0;
         for (int i = 0; i < NUM_ALLOCS; i++) {
-                int* ptr =
-                    reinterpret_cast<int*>(anvil::memory::stack_allocator::alloc(allocator, ALLOC_SIZE, alignof(int)));
+                int* ptr = reinterpret_cast<int*>(anvil::memory::lazy_stack_allocator::alloc(allocator, ALLOC_SIZE,
+                                                                                             alignof(int)));
 
                 if (ptr == nullptr) {
                         printf("ALLOCATION FAILURE at %i\n", i);
