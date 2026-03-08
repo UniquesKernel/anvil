97ee5372300a6bc633f12d32a3d6fc21c03e9977
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2026-03-08 23:30:31 +0100

    poison malloc, realloc, calloc and free

---
+++ include/memory/memory_policy.hpp
@@ -0,0 +1,9 @@
+#ifndef ANVIL_MEMORY_POLICY_HPP
+#define ANVIL_MEMORY_POLICY_HPP
+
+#include <cstdlib>
+#include <cstring>
+
+#pragma GCC poison malloc calloc realloc free
+
+#endif // !ANVIL_MEMORY_POLICY_HPP
