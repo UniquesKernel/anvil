4c44817c88e612398c3dc0a206c1d130cdf006de
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2026-03-11 19:20:27 +0100

    fixed tests which are not actually properly writen

--- include/memory/constants.hpp
+++ include/memory/constants.hpp
@@ -8,13 +8,12 @@
 
 namespace anvil::memory {
 
-static const size_t      PAGE_SIZE       = (size_t)(sysconf(_SC_PAGESIZE));
-
+inline const std::size_t PAGE_SIZE       = (size_t)(sysconf(_SC_PAGESIZE));
 inline const std::size_t MAX_ALIGNMENT   = PAGE_SIZE >> 1; // NOLINT alignment is capped at half a page.
 inline const std::size_t MIN_ALIGNMENT   = 1;
 inline const std::size_t MAX_STACK_DEPTH = 64;
 inline const std::size_t MAX_CAPACITY    = SIZE_MAX / 2;
-static const size_t      PAGE_SHIFT      = (unsigned long)(__builtin_ctzl(PAGE_SIZE));
+inline const std::size_t PAGE_SHIFT      = (unsigned long)(__builtin_ctzl(PAGE_SIZE));
 
 } // namespace anvil::memory
 #endif // ANVIL_MEMORY_CONSTANTS_HPP
