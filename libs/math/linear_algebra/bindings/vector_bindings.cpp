921987005b7c7a84d4f9db293edbc5936818df89
UniquesKernel <85461364+UniquesKernel@users.noreply.github.com>
2026-03-15 17:31:20 +0100

    fixed some usages of namespacing for better binding code in math library linear_algebra

--- libs/math/linear_algebra/bindings/vector_bindings.cpp
+++ libs/math/linear_algebra/bindings/vector_bindings.cpp
@@ -9,17 +9,17 @@ PYBIND11_MODULE(math_module, module) {
         py::class_<anvil::math::Vector2>(module, "Vector2")
             .def(py::init([](const anvil::f32& a, const anvil::f32& b) { return anvil::math::Vector2{a, b}; }))
             .def_property(
-                "x", [](anvil::math::Vector2& v) { return pybind11::module_::import("numpy").attr("float32")(v.x); },
+                "x", [](anvil::math::Vector2& v) { return py::module_::import("numpy").attr("float32")(v.x); },
                 [](anvil::math::Vector2& v, anvil::f32 val) { v.x = val; })
             .def_property(
-                "y", [](anvil::math::Vector2& v) { return pybind11::module_::import("numpy").attr("float32")(v.y); },
+                "y", [](anvil::math::Vector2& v) { return py::module_::import("numpy").attr("float32")(v.y); },
                 [](anvil::math::Vector2& v, anvil::f32 val) { v.y = val; })
             .def("__add__", [](const anvil::math::Vector2& a, const anvil::math::Vector2& b) { return a + b; })
             .def("__sub__", [](const anvil::math::Vector2& a, const anvil::math::Vector2& b) { return a - b; })
             .def("__neg__", [](const anvil::math::Vector2& a) { return -a; })
             .def("__mul__",
                  [](const anvil::math::Vector2& a, const anvil::math::Vector2& b) {
-                         return pybind11::module_::import("numpy").attr("float32")(a * b);
+                         return py::module_::import("numpy").attr("float32")(a * b);
                  })
             .def("__mul__", [](const anvil::math::Vector2& a, const anvil::f32 scalar) { return a * scalar; })
             .def("__rmul__", [](const anvil::math::Vector2& a, const anvil::f32 scalar) { return scalar * a; })
@@ -30,20 +30,20 @@ PYBIND11_MODULE(math_module, module) {
                     return anvil::math::Vector3{a, b, c};
             }))
             .def_property(
-                "x", [](anvil::math::Vector3& v) { return pybind11::module_::import("numpy").attr("float32")(v.x); },
+                "x", [](anvil::math::Vector3& v) { return py::module_::import("numpy").attr("float32")(v.x); },
                 [](anvil::math::Vector3& v, anvil::f32 val) { v.x = val; })
             .def_property(
-                "y", [](anvil::math::Vector3& v) { return pybind11::module_::import("numpy").attr("float32")(v.y); },
+                "y", [](anvil::math::Vector3& v) { return py::module_::import("numpy").attr("float32")(v.y); },
                 [](anvil::math::Vector3& v, anvil::f32 val) { v.y = val; })
             .def_property(
-                "z", [](anvil::math::Vector3& v) { return pybind11::module_::import("numpy").attr("float32")(v.z); },
+                "z", [](anvil::math::Vector3& v) { return py::module_::import("numpy").attr("float32")(v.z); },
                 [](anvil::math::Vector3& v, anvil::f32 val) { v.z = val; })
             .def("__add__", [](const anvil::math::Vector3& a, const anvil::math::Vector3& b) { return a + b; })
             .def("__sub__", [](const anvil::math::Vector3& a, const anvil::math::Vector3& b) { return a - b; })
             .def("__neg__", [](const anvil::math::Vector3& a) { return -a; })
             .def("__mul__",
                  [](const anvil::math::Vector3& a, const anvil::math::Vector3& b) {
-                         return pybind11::module_::import("numpy").attr("float32")(a * b);
+                         return py::module_::import("numpy").attr("float32")(a * b);
                  })
             .def("__mul__", [](const anvil::math::Vector3& a, const anvil::f32 scalar) { return a * scalar; })
             .def("__rmul__", [](const anvil::math::Vector3& a, const anvil::f32 scalar) { return scalar * a; })
